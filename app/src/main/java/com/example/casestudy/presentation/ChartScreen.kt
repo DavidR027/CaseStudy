e631261d5d90c07eaf1b60355f047a9b54f878db
BluB3ry <davidraharja910@gmail.com>
2023-08-20 21:48:31 +0700

    Removed Imports

--- app/src/main/java/com/example/casestudy/presentation/ChartScreen.kt
+++ app/src/main/java/com/example/casestudy/presentation/ChartScreen.kt
@@ -1,13 +1,9 @@
 package com.example.casestudy.presentation
 
-import android.os.DropBoxManager
-import androidx.compose.foundation.Canvas
 import androidx.compose.foundation.layout.Box
-import androidx.compose.foundation.layout.BoxWithConstraints
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.lazy.LazyColumn
@@ -26,7 +22,6 @@ import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.unit.dp
-import androidx.compose.ui.viewinterop.AndroidView
 import androidx.navigation.NavController
 import com.example.casestudy.data.dc.DonutChartData
 import com.example.casestudy.domain.repo.ChartRepository
